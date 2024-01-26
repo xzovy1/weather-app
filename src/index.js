1be88fd592ec67472a961aae51edf3e909e49c29
cabelstein12 <Caleb.Steinwandt@gmail.com>
2024-01-26 05:11:44 -0700

    project initialize

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+import background from './assets/iccup-Rljwy2QK4YE-unsplash.jpg';
+
+const body = document.querySelector('body');
+body.style.backgroundImage = `url(${background})`;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode:'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+    },
+    target: 'web',
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname,"dist")
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        }),
+        new BundleAnalyzerPlugin()
+    ],
+}
