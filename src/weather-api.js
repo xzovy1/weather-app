041a5661cea46cdab347a31b8e93a6d01b75104a
cabelstein12 <Caleb.Steinwandt@gmail.com>
2024-01-29 19:26:08 -0700

    change to https:

--- src/weather-api.js
+++ src/weather-api.js
@@ -1,5 +1,5 @@
 export async function fetchWeather(location = 'edmonton'){
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=993d595687db4a4dad2153340242401&q=${location}&aqi=no`, {mode: 'cors'})
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=993d595687db4a4dad2153340242401&q=${location}&aqi=no`, {mode: 'cors'})
     .then(function(response){return response.json()})
     .then(function(response){return response})
     return response;
